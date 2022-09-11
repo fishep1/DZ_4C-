6db18cbc2e338a07b39ee4b2dd17ce791bf91d37
Nikita Pletnev <betpezeh1@yahoo.com>
2022-09-11 09:30:29 +0500

    add Example 001

---
+++ Example001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число А: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+for(int i = b; i <= b; i++)
+    Console.WriteLine($"{a}^{b} = {Math.Pow(a, b)}");
+
